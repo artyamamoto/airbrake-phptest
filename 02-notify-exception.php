dbe00be3a9b1b13cbd4b31eea8d1d35c0c900a30
artyamamoto <yamamoto.k@art-bank.co.jp>
2014-02-12 17:46:05 +0900

    Airbrake.io 側のAPIキーの更新

--- 02-notify-exception.php
+++ 02-notify-exception.php
@@ -5,8 +5,11 @@ $options = array();
 $config = new Airbrake\Configuration(AIRBRAKE_API_KEY, $options);
 $client = new Airbrake\Client($config);
 
+include __DIR__."/lib/TestException01.php";
+
+
 try {
-	throw new Exception('something is wrong...02');
+	throw new TestException01('something is wrong...02');
 } catch(Exception $e) {
 	$client->notifyOnException($e);
 }	
